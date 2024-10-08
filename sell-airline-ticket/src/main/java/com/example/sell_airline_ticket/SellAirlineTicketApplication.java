dec6e69ba78ead57311b8c82b663803a08b5ad19
XuanNam <dangxuannam58@gmail.com>
2024-10-08 17:09:04 +0700

    create new packages

---
+++ sell-airline-ticket/src/main/java/com/example/sell_airline_ticket/SellAirlineTicketApplication.java
@@ -0,0 +1,13 @@
+package com.example.sell_airline_ticket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SellAirlineTicketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SellAirlineTicketApplication.class, args);
+	}
+
+}
---
+++ sell-airline-ticket/src/test/java/com/example/sell_airline_ticket/SellAirlineTicketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sell_airline_ticket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SellAirlineTicketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
